7974ebcbf4262316def1d0fb8780b88ac78aeaa8
Matheus2563 <mb1699114@gmail.com>
2025-12-04 22:56:07 -0300

    resolvendo_merge

--- match.up/app.py
+++ match.up/app.py
@@ -219,6 +219,7 @@ def logout():
 # ATUALIZAÇÃO DO PERFIL
 # -----------------
 
+# passo 1 ---------------------
 def get_all_users_from_csv():
     rows = []
     if os.path.exists(CSV_FILENAME):
@@ -260,7 +261,7 @@ def handle_profile_picture_upload(file):
             return foto_perfil_path, f"Erro ao fazer upload da foto: {e}"
 
     return foto_perfil_path, None
-
+# passo 2 --------------------- 
 def update_user_in_csv(old_email, new_nome, new_email, new_password, new_foto_perfil):
     all_users = get_all_users_from_csv()
     updated = False
@@ -298,7 +299,7 @@ def update_user_in_csv(old_email, new_nome, new_email, new_password, new_foto_pe
             return False, "Erro desconhecido ao recarregar o perfil após atualização."
 
     return False, "Usuário não encontrado para atualização."
-
+# passo 3 ----------------------------------
 @app.route('/editar_perfil', methods=['GET'])
 @login_required
 def editar_perfil():
